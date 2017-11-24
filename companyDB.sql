bf255c74199a3bee15057db7b58be9614b6e96a5
Shivang Belwariar <belwariars@gmail.com>
2017-11-24 20:42:20 +0530

    Create companyDB.sql

---
+++ companyDB.sql
@@ -0,0 +1,29 @@
+INSERT INTO company_details (name,company_id,test_date,interview_date,tier,website,postal_address,company_sector)
+VALUES ('Citrix', 01, '2017-10-10', '2013-10-11', 1, 'www.citrix.com', 'Addr1', 'IT' );
+
+INSERT INTO CUSTOMERS (name,company_id,test_date,interview_date,tier,website,postal_address,company_sector)
+VALUES ('Endurance', 02, '2017-10-15', '2013-10-16', 1, 'www.endurance.com', 'Addr2', 'IT' );
+
+INSERT INTO CUSTOMERS (name,company_id,test_date,interview_date,tier,website,postal_address,company_sector)
+VALUES ('Akamai', 03, '2017-10-20', '2013-10-21', 1, 'www.akamai.com', 'Addr3', 'IT' );
+
+INSERT INTO CUSTOMERS (name,company_id,test_date,interview_date,tier,website,postal_address,company_sector)
+VALUES ('Accenture', 04, '2017-11-10', '2013-11-11', 3, 'www.accenture.com', 'Addr4', 'IT' );
+
+INSERT INTO CUSTOMERS (name,company_id,test_date,interview_date,tier,website,postal_address,company_sector)
+VALUES ('Bosch', 05, '2017-11-14', '2013-11-15', 2, 'www.bosch.com', 'Addr5', 'Non-IT' );
+
+INSERT INTO CUSTOMERS (name,company_id,test_date,interview_date,tier,website,postal_address,company_sector)
+VALUES ('SAP Labs', 06, '2017-11-17', '2013-11-18', 1, 'www.sap.com', 'Addr6', 'IT' );
+
+INSERT INTO CUSTOMERS (name,company_id,test_date,interview_date,tier,website,postal_address,company_sector)
+VALUES ('Samsung', 07, '2017-11-20', '2013-11-21', 1, 'www.samsungrnd.com', 'Addr7', 'IT' );
+
+INSERT INTO CUSTOMERS (name,company_id,test_date,interview_date,tier,website,postal_address,company_sector)
+VALUES ('Cognizant', 08, '2017-11-22', '2013-11-23', 3, 'www.cognizant.com', 'Addr8', 'Sales' );
+
+INSERT INTO CUSTOMERS (name,company_id,test_date,interview_date,tier,website,postal_address,company_sector)
+VALUES ('Razor Pay', 09, '2017-11-25', '2013-11-26', 1, 'www.razorpay.com', 'Addr9', 'IT' );
+
+INSERT INTO CUSTOMERS (name,company_id,test_date,interview_date,tier,website,postal_address,company_sector)
+VALUES ('Deloitte', 10, '2017-11-27', '2013-11-28', 1, 'www.deloitte.com', 'Addr10', 'Consultancy' );
