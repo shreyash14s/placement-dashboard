5f8adc450457b032fedb5dace89226e9dd5bf3b8
Shreyash S Sarnayak <shreyash14s@gmail.com>
2017-11-26 23:30:21 +0530

    Added controller for funtionality

--- PlacementApp/app/__init__.py
+++ PlacementApp/app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 fapp = Flask(__name__)
 
 import app.views
+from app.models import Student, Company
 
 @fapp.errorhandler(404)
 def not_found(error):
---
+++ PlacementApp/app/controller.py
@@ -0,0 +1,37 @@
+from app.models import StudentUser
+
+def signup_student(user, pwd):
+    '''
+    Create new student user.
+    '''
+    pass
+
+def login_student(user, pwd):
+    '''
+    Returns True if the username and password match.
+    '''
+    pass
+
+def get_all_companies():
+    '''
+    Returns all the companies list available for registration.
+    '''
+    pass
+
+def get_student_details(usn):
+    '''
+    Returns the student details.
+    '''
+    pass
+
+def register_for(usn, company_id):
+    '''
+    Register the student for company.
+    '''
+    pass
+
+def get_excel_sheet(company_id):
+    '''
+    Create an excel sheet for company.
+    '''
+    pass
