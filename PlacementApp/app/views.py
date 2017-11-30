e5f558c089d9ea57665e67ba91cf593fdcbd98f1
SanjanaJairam <33804996+SanjanaJairam@users.noreply.github.com>
2017-11-30 11:17:25 +0530

    Update views.py

--- PlacementApp/app/views.py
+++ PlacementApp/app/views.py
@@ -1,5 +1,6 @@
 from app import fapp
 from app.query import gen_excel_sheets
+from app.controller import get_student_details
 from flask import send_from_directory
 from config import EXCEL_FILES_DIR
 
@@ -12,3 +13,8 @@ def get_excel(comp_id):
     filename = gen_excel_sheets(comp_id)
     if filename:
         return send_from_directory(EXCEL_FILES_DIR, filename, as_attachment=True)
+
+@fapp.route("/student_details/<usn>", methods=['GET'])
+def get_student_details(usn):
+    details = get_student_details(usn)
+    return details
