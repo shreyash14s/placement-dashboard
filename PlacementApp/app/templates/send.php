4171747909086d22c9e33d8fac707fcd0a3de334
Sai Neeraj Kanuri <neeraj@Sais-MacBook-Pro.local>
2017-12-01 10:32:51 +0530

    ok

---
+++ PlacementApp/app/templates/send.php
@@ -0,0 +1,22 @@
+<?php
+require_once 'swift/lib/swift_required.php';
+
+$transport = Swift_SmtpTransport::newInstance('smtp.gmail.com', 465, "ssl")
+  ->setUsername('pesplacementhub@gmail.com')
+  ->setPassword('aabbccdd123');
+
+$emails = ['pesplacementhub1@gmail.com','pesplacementhub2@gmail.com'];
+
+foreach ($emails as $email) {
+    $mailer = Swift_Mailer::newInstance($transport);
+
+	$message = Swift_Message::newInstance('PES Placement Hub - ')
+	  ->setFrom(array('pesplacementhub@gmail.com' => 'PES Placement Portal'))
+	  ->setTo(array($email))
+	  ->setBody('Welcome to PES Placement Portal'.PHP_EOL.'Dear student,'.PHP_EOL."New companies have been added to the portal recently. Login at http://localhost:8080 to register!".PHP_EOL.PHP_EOL."Regards,".PHP_EOL."Team Placement Hub");
+
+	$result = $mailer->send($message);
+} 
+
+
+?>
